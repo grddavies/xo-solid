9ed23b0d901e31b3b47389a756b4d78c1b0c8338
Gethin Davies <grd.davies@gmail.com>
2021-09-21 14:31:48 +0100

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import type { Component } from "solid-js";
 
-import { Row, Game, Container, Col } from "./components";
+import { Row, Game, Navbar, Col } from "./components";
 
 const App: Component = () => {
   return (
-    <Container fluid={true}>
-      <Row>
-        <Col width="6" className="offset-3">
-          <h1 className="text-center">Noughts & Crosses</h1>
-          <Game></Game>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Navbar></Navbar>
+      <main role="main" className="container">
+        <Row>
+          <Col width="6" className="offset-3">
+            <h1 className="text-center">Noughts & Crosses</h1>
+              <Game></Game>
+          </Col>
+        </Row>
+      </main>
+    </>
   );
 };
 
